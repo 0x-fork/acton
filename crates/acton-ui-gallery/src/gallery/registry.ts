f4597b39a30ee445659d781f614e71916055a7a7
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-07-04 13:43:25 +0400

    feat(ui): add base UI package with initial 13 common components

---
+++ crates/acton-ui-gallery/src/gallery/registry.ts
@@ -0,0 +1,30 @@
+import {buttonGallery} from "./buttonGallery"
+import {breadcrumbsGallery} from "./breadcrumbsGallery"
+import {checkboxGallery} from "./checkboxGallery"
+import {contentTabsGallery} from "./contentTabsGallery"
+import {dataTableGallery} from "./dataTableGallery"
+import {disclosureToggleGallery} from "./disclosureToggleGallery"
+import {inlineActionsGallery} from "./inlineActionsGallery"
+import {inlineButtonGallery} from "./inlineButtonGallery"
+import {markdownTextGallery} from "./markdownTextGallery"
+import {pillTabsGallery} from "./pillTabsGallery"
+import {rawDataBlockGallery} from "./rawDataBlockGallery"
+import {skeletonGallery} from "./skeletonGallery"
+import {themeSwitchGallery} from "./themeSwitchGallery"
+import type {ComponentGallery} from "./types"
+
+export const galleries = [
+  buttonGallery,
+  breadcrumbsGallery,
+  inlineButtonGallery,
+  inlineActionsGallery,
+  disclosureToggleGallery,
+  contentTabsGallery,
+  pillTabsGallery,
+  markdownTextGallery,
+  rawDataBlockGallery,
+  dataTableGallery,
+  skeletonGallery,
+  checkboxGallery,
+  themeSwitchGallery,
+] satisfies readonly ComponentGallery[]
