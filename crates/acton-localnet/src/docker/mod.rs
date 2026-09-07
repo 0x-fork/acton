ff2853dc23ec545edfebcee3118adf6f491fc295
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-09-07 11:54:55 +0400

    chore(localton): update default image

--- crates/acton-localnet/src/docker/mod.rs
+++ crates/acton-localnet/src/docker/mod.rs
@@ -10,7 +10,7 @@ use crate::{Error, Network, NetworkConfig};
 
 const COMPOSE_TEMPLATE: &str = include_str!("../../assets/localton.compose.yaml");
 const DEFAULT_LOCALTON_IMAGE: &str =
-    "ghcr.io/ton-blockchain/localton:sha-0394fbfe91cebd55d893ef8fe7433d7b4f1700e9";
+    "ghcr.io/ton-blockchain/localton:sha-1cae3c73a0bfe69ea19bd82b047190a44a0582d0";
 const COMPOSE_WAIT_TIMEOUT_SECONDS: u16 = 600;
 const DOCKER_CONFIG_DIRECTORY: &str = "docker-pull-config";
 const RUNTIME_DESCRIPTOR_FILE: &str = "runtime.json";
